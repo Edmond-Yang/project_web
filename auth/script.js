3b9073ab5d92a3bdd8b85651c01ab88798b0a546
Edmond-Yang <62778632+Edmond-Yang@users.noreply.github.com>
2022-12-18 13:32:54 +0800

    intial webAuthn and add some features

--- auth/script.js
+++ auth/script.js
@@ -1,6 +1,20 @@
+class local_storage{
 
+    getter(key){
+        return JSON.stringify(localStorage.getItem(key));
+    }
+
+    setter(key, value){
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+}
+
+const storage = new local_storage();
 
 window.onload = function(){
+
+    // login and sign up switch
     const switchers = [...document.querySelectorAll('.switcher')]
     switchers.forEach(item =>{
         item.addEventListener('click', function(){
@@ -10,4 +24,9 @@ window.onload = function(){
             this.parentElement.classList.add('is-active')
         })
     })
+
+    // from local storage
+    const value = storage.getter('user-email');
+    if(value)
+        $('#login-email').val(value);
 } 
