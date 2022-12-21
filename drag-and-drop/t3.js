d63b1673f2f01692ae7875c0bca0a296bfc2d2c0
daaaaaaavid <b0932296807@gmail.com>
2022-12-21 22:29:32 +0800

    add draggable images & text

--- drag-and-drop/t3.js
+++ drag-and-drop/t3.js
@@ -1,59 +1,109 @@
 window.addEventListener("DOMContentLoaded", () => {
-    slist(document.getElementById("sortlist"));
-  });
-function slist (target) {
-    // (A) SET CSS + GET ALL LIST ITEMS
-    target.classList.add("slist");
-    let items = target.getElementsByTagName("div"), current = null;
-  
-    // (B) MAKE ITEMS DRAGGABLE + SORTABLE
-    for (let i of items) {
-      // (B1) ATTACH DRAGGABLE
-      i.draggable = true;
-  
-      // (B2) DRAG START - YELLOW HIGHLIGHT DROPZONES
-      i.ondragstart = (ev) => {
-        current = i;
-        for (let it of items) {
-          if (it != current) { it.classList.add("hint"); }
-        }
-      };
-  
-      // (B3) DRAG ENTER - RED HIGHLIGHT DROPZONE
-      i.ondragenter = (ev) => {
-        if (i != current) { i.classList.add("active"); }
-      };
-  
-      // (B4) DRAG LEAVE - REMOVE RED HIGHLIGHT
-      i.ondragleave = () => {
-        i.classList.remove("active");
-      };
-  
-      // (B5) DRAG END - REMOVE ALL HIGHLIGHTS
-      i.ondragend = () => { for (let it of items) {
-        it.classList.remove("hint");
-        it.classList.remove("active");
-      }};
-  
-      // (B6) DRAG OVER - PREVENT THE DEFAULT "DROP", SO WE CAN DO OUR OWN
-      i.ondragover = (evt) => { evt.preventDefault(); };
-  
-      // (B7) ON DROP - DO SOMETHING
-      i.ondrop = (evt) => {
-        evt.preventDefault();
-        if (i != current) {
-          let currentpos = 0, droppedpos = 0;
-          for (let it=0; it<items.length; it++) {
-            if (current == items[it]) { currentpos = it; }
-            if (i == items[it]) { droppedpos = it; }
-          }
-          if (currentpos < droppedpos) {
-            i.parentNode.insertBefore(current, i.nextSibling);
-          } else {
-            i.parentNode.insertBefore(current, i);
-          }
-        }
-      };
+  add_template_event();
+});
+function add_template_event(){
+
+  let items = document.getElementsByClassName("template"), current = null;
+
+  for(let i of items){
+    i.draggable = true;
+
+    i.ondragstart = (ev) => {
+      if(i.id=="text-only")
+        ev.dataTransfer.setData("type","text-only");
+      else if(i.id=="image-only")
+        ev.dataTransfer.setData("type","image-only");
+
+      var blocks_end = document.createElement("div");
+
+      blocks_end.className = "block" ;
+      current = blocks_end;
+      blocks_end.classList.add("block_end");
+      document.getElementById("sortlist").appendChild(blocks_end);
+
+      add_block_event();
+
+      let blocks = document.getElementsByClassName("block");
+      for(let j of blocks){
+        j.classList.add("hint");
+      }
+    }
+
+    i.ondragend = (ev) =>{
+      let blocks = document.getElementsByClassName("block");
+      for(let j of blocks){
+        j.classList.remove("hint");
+      }
+      let block_end = document.getElementsByClassName("block_end")[0];
+      if(block_end){
+        document.getElementById("sortlist").removeChild(block_end);
+      }
     }
   }
+}
+function add_block_event(){
+  let blocks = document.getElementsByClassName("block");
+
+  for(let i of blocks){
+
+    i.ondragenter = (ev) =>{
+      i.classList.add("active");
+    }
+    i.ondragleave = (ev) =>{
+      i.classList.remove("active");
+    }
+    i.ondragover = (ev) =>{
+      ev.preventDefault();
+    }
+    i.ondrop = (ev) =>{
+      ev.preventDefault();
+      let current = document.getElementsByClassName("block_end")[0];
+      let items = document.getElementsByClassName("block");
+
+      if (i != current) {
+        let currentpos = -1, droppedpos = -1;
+        for (let it=0; it<items.length; it++) {
+          if (current == items[it]) { currentpos = it; }
+          if (i == items[it]) { droppedpos = it; }
+        }
+
+        if (currentpos < droppedpos) {
+          i.parentNode.insertBefore(current, i.nextSibling);
+        } else {
+          i.parentNode.insertBefore(current, i);
+        }
+      }
+      current.classList.remove("block_end");
+      i.classList.remove("active");
+
+      if(ev.dataTransfer.getData("type")=="text-only") current.contentEditable = "true";
+      else if(ev.dataTransfer.getData("type")=="image-only"){
+        let input = document.createElement("input");
+        input.type = "file";
+        input.accept = "image/*";
+        input.name = "img";
+        input.addEventListener("change", function() {
+
+          const reader = new FileReader()
+        
+          reader.addEventListener("load", () => {
+            //document.querySelector("#image").src = reader.result
+            let parent = input.parentElement;
+            parent.removeChild(input);
+            let image = document.createElement("img");
+            image.width = "200";
+            image.src = reader.result;
+            parent.appendChild(image);
+          })
+        
+          reader.readAsDataURL(this.files[0])
+        
+        })
+        current.appendChild(input);
+      }
+    }
+  }
+}
+
+
   
