cf047382aa9c8e25bbd7bede06bf39987c3bbeea
Edmond-Yang <62778632+Edmond-Yang@users.noreply.github.com>
2022-12-22 15:19:38 +0800

    complete simple creation page

---
+++ creation_project/control.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    $('#user').on('click', function(){
+        
+        if($('.user-list').css('display') == 'none')
+            $('.user-list').css('display', 'block');
+        else
+            $('.user-list').css('display', 'none');
+
+    })
+}
