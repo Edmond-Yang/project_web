0d032a04c8790c915baeb84808a82b663155693e
Edmond-Yang <62778632+Edmond-Yang@users.noreply.github.com>
2022-12-26 19:16:44 +0800

    add update.php to update to mysql

---
+++ drag-and-drop/update.php
@@ -0,0 +1,17 @@
+<?php 
+
+    if(!isset($_POST['email']) || !isset($_POST['project']) || !isset($_POST['html'])){
+        echo 'get away';
+        die();
+    }
+
+    if (!($database = mysqli_connect("localhost", "admin", "WEB#dreamer$1104&1126", "web_dreamer"))) {
+        die("Connection failed.");
+    }
+
+    $sql = "UPDATE `" . $_POST['email'] . "` SET `html` = '" . $_POST['html'] . "' WHERE `" . $_POST['email'] . "`.`id` = " . $_POST['project'];
+    if (!($query = mysqli_query($database, $query))) {
+        die("INSERTION failed.");
+    }
+
+?>
